f578b857fe80bfa88b9d080509206884ffcd9ef7
elsa-nishori <elsa.nishori1@gmail.com>
2022-07-02 23:05:15 +0200

    Playground Assignment

---
+++ Python_Fundamentals/flask/playground/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/play')
+def play():
+    return render_template("index.html", num=3, color='aqua')
+
+@app.route('/play/<int:num>')
+def play_num(num):
+    return render_template("index.html", num=num, color='aqua')
+
+@app.route('/play/<int:num>/<string:color>')
+def play_num_color(num, color):
+    return render_template("index.html", num=num, color=color)
+
+if __name__ == "__main__":
+    app.run(debug=True)
